956bc7f311e7503211645e3600c4dd0ce31e9cb9
Manjunath LD <ldmanju007@gmail.com>
2022-02-16 23:03:47 +0530

    Commit006: Added on Item click listener to Country RecyclerView

--- app/src/main/java/com/example/mpv_kotlin_based/MainActivity.kt
+++ app/src/main/java/com/example/mpv_kotlin_based/MainActivity.kt
@@ -3,18 +3,14 @@ package com.example.mpv_kotlin_based
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mpv_kotlin_based.adapter.CountriesAdapter
-import com.example.mpv_kotlin_based.model.CountriesService
 import com.example.mpv_kotlin_based.model.CountryModel
 import com.example.mpv_kotlin_based.presenter.CountryPresenter
-import okhttp3.internal.notifyAll
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-class MainActivity : AppCompatActivity(), CountryPresenter.View {
+class MainActivity : AppCompatActivity(), CountryPresenter.View, CountriesAdapter.CountriesViewHolderListener {
 
     private lateinit var rvCountries: RecyclerView
     private lateinit var countriesAdapter: CountriesAdapter
@@ -29,11 +25,10 @@ class MainActivity : AppCompatActivity(), CountryPresenter.View {
 
         CountryPresenter(this)
 
-        countriesAdapter = CountriesAdapter(listOfCountriesModel)
+        countriesAdapter = CountriesAdapter(listOfCountriesModel, this)
         val linearLayoutManager: LinearLayoutManager = LinearLayoutManager(this)
         rvCountries.layoutManager = linearLayoutManager
         rvCountries.adapter = countriesAdapter
-
 
     }
 
@@ -44,6 +39,23 @@ class MainActivity : AppCompatActivity(), CountryPresenter.View {
     }
 
     override fun onError(error: Throwable) {
-        Log.e("onError", ""+error.toString())
+        Log.e("onError", "" + error.toString())
     }
+
+    override fun onItemClick(
+        countriesViewHolder: CountriesAdapter.CountriesViewHolder
+    ) {
+        val position: Int = countriesViewHolder.adapterPosition
+        val countriesID: String = listOfCountriesModel[position].countryCode
+        val countryName: String = listOfCountriesModel[position].countryName
+        val countryDate: String = listOfCountriesModel[position].countryDate
+        Toast.makeText(
+            this,
+            "Item clicked\n"+
+                    "Country ID: $countriesID\n"+
+                    "Country Name: $countryName\n"+
+                    "Country Date: $countryDate", Toast.LENGTH_SHORT
+        ).show()
+    }
+
 }
--- app/src/main/java/com/example/mpv_kotlin_based/adapter/CountriesAdapter.kt
+++ app/src/main/java/com/example/mpv_kotlin_based/adapter/CountriesAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.mpv_kotlin_based.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +8,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mpv_kotlin_based.R
 import com.example.mpv_kotlin_based.model.CountryModel
 
-class CountriesAdapter(listCountriesModel: List<CountryModel>) :
+class CountriesAdapter(listCountriesModel: List<CountryModel>, countriesViewHolderListener: CountriesViewHolderListener) :
     RecyclerView.Adapter<CountriesAdapter.CountriesViewHolder>() {
 
-    private val localListCountriesModel : List<CountryModel> = listCountriesModel
+    private lateinit var localListCountriesModel : List<CountryModel>
+    private lateinit var countriesViewHolderListener: CountriesViewHolderListener
+
+    init {
+        this.countriesViewHolderListener = countriesViewHolderListener
+        this.localListCountriesModel = listCountriesModel
+    }
 
     class CountriesViewHolder(countryItemView: View) : RecyclerView.ViewHolder(countryItemView) {
         private val tvCountryCode: TextView = countryItemView.findViewById(R.id.tv_CountryCode)
@@ -29,20 +34,27 @@ class CountriesAdapter(listCountriesModel: List<CountryModel>) :
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): CountriesAdapter.CountriesViewHolder {
+    ): CountriesViewHolder {
         val countryRowItemView: View = LayoutInflater.from(parent.context)
             .inflate(R.layout.country_row_item, parent, false)
 
-        return CountriesAdapter.CountriesViewHolder(countryRowItemView)
+        return CountriesViewHolder(countryRowItemView)
     }
 
-    override fun onBindViewHolder(holder: CountriesAdapter.CountriesViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: CountriesViewHolder, position: Int) {
         holder.bindData(localListCountriesModel[position])
+        holder.itemView.setOnClickListener(View.OnClickListener {
+            countriesViewHolderListener.onItemClick(holder)
+        })
     }
 
     override fun getItemCount(): Int {
         return if (localListCountriesModel.isNotEmpty()) {
             localListCountriesModel.size
         } else { 0 }
+    }
+
+    public interface CountriesViewHolderListener {
+        fun onItemClick(countriesViewHolder: CountriesViewHolder)
     }
 }
